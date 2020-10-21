199b8d676a7c8e875a4643376d4e401998aa09d7
osucubs <zwiford@icloud.com>
2020-10-21 14:55:44 -0400

    updated to core 3.1

--- Claims/Startup.cs
+++ Claims/Startup.cs
@@ -10,85 +10,92 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Claims
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddIdentity<IdentityUser, IdentityRole>(options =>
-            {
-                options.Password.RequireDigit = false;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequiredLength = 6;
-            })
-               .AddDefaultUI(UIFramework.Bootstrap4)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("WorkedTwoYears", policy =>
-                    policy.Requirements.Add(new MinimumYearsWorkedRequirement(2)));
-
-                options.AddPolicy("WorkedFiveYears", policy =>
-                    policy.Requirements.Add(new MinimumYearsWorkedRequirement(5)));
-
-                options.AddPolicy("WorkedTenYears", policy =>
-                    policy.Requirements.Add(new MinimumYearsWorkedRequirement(10)));
-            });
-
-            services.AddSingleton<IAuthorizationHandler, YearsWorkedHandler>();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
-
-            app.UseAuthentication();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.Configure<CookiePolicyOptions>(options => {
+                                                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                                                options.CheckConsentNeeded = context => true;
+                                                options.MinimumSameSitePolicy = SameSiteMode.None;
+                                              });
+
+      services.AddDbContext<ApplicationDbContext>(options =>
+                                                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+      services.AddIdentity<IdentityUser, IdentityRole>(options => {
+                                                         options.Password.RequireDigit = false;
+                                                         options.Password.RequireNonAlphanumeric = false;
+                                                         options.Password.RequireUppercase = false;
+                                                         options.Password.RequiredLength = 6;
+                                                       })
+              .AddDefaultUI()
+              .AddEntityFrameworkStores<ApplicationDbContext>();
+
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
+
+      services.AddAuthorization(options => {
+                                  options.AddPolicy("WorkedTwoYears", policy =>
+                                                                        policy.Requirements.Add(new MinimumYearsWorkedRequirement(2)));
+
+                                  options.AddPolicy("WorkedFiveYears", policy =>
+                                                                         policy.Requirements.Add(new MinimumYearsWorkedRequirement(5)));
+
+                                  options.AddPolicy("WorkedTenYears", policy =>
+                                                                        policy.Requirements.Add(new MinimumYearsWorkedRequirement(10)));
+                                });
+
+      services.AddSingleton<IAuthorizationHandler, YearsWorkedHandler>();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+
+      app.UseRouting();
+
+      app.UseAuthentication();
+      app.UseAuthorization();
+
+      //*************************
+      // Routing - AREAS
+      //*************************
+      app.UseEndpoints(endpoints => {
+                         endpoints.MapControllerRoute(name: "areas",
+                                                      pattern: "{area:exists}/{controller}/{action}/{id?}");
+
+                         endpoints.MapControllerRoute(name: "default",
+                                                      pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                         endpoints.MapControllers(); // enables controllers in endpoint routing
+                         endpoints.MapDefaultControllerRoute(); // adds the default route {controller=Home}/{action=Index}/{id?}
+
+                         endpoints.MapRazorPages();
+                       });
+    }
+  }
 }
